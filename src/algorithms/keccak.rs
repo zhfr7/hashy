bea507f181c1176be4f04256c86cf7beb69429b0
Ahmad Zhafir <zzzhafir75@gmail.com>
2021-10-16 19:39:10 +0200

    Implement 3 step mapping functions
    Add Lanes type alias. The functions are not yet tested.

--- src/algorithms/keccak.rs
+++ src/algorithms/keccak.rs
@@ -2,8 +2,10 @@
 // - https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf
 // - https://github.com/ctz/keccak/blob/master/keccak.py
 
+type Lanes = [[u64; 5]; 5];
+
 struct KeccakState {
-    lanes: [[u64; 5]; 5]
+    lanes: Lanes
 }
 
 impl KeccakState {
@@ -20,16 +22,8 @@ impl KeccakState {
         for y in 0..5 {
             for x in 0..5 {
                 out.lanes[x][y] = u64::from_be_bytes(
-                    [
-                        bytes[i],
-                        bytes[i+1],
-                        bytes[i+2],
-                        bytes[i+3],
-                        bytes[i+4],
-                        bytes[i+5],
-                        bytes[i+6],
-                        bytes[i+7],
-                    ]
+                    [bytes[i], bytes[i+1], bytes[i+2], bytes[i+3],
+                    bytes[i+4], bytes[i+5], bytes[i+6], bytes[i+7]]
                 );
 
                 i += 8;
@@ -50,6 +44,65 @@ impl KeccakState {
         }
 
         out
+    }
+
+    // Step mapping functions
+
+    pub fn phi(&mut self) {
+        let mut c = vec![];
+        let mut d = vec![];
+
+        for x in 0..5 {
+            c.push(self.lanes[x].iter().fold(0, 
+                |current, lane| current & lane));
+        }
+
+        for x in 0..5 {
+            d.push(c[(x-1) % 5] ^ c[(x+1) % 5].rotate_right(1));
+        }
+
+        for x in 0..5 {
+            for y in 0..5 {
+                self.lanes[x][y] ^= d[x];
+            }
+        }
+    }
+
+    pub fn rho(&mut self) {
+        let (mut x, mut y) = (1, 0);
+
+        for t in 0..24 {
+            self.lanes[x][y] = self.lanes[x][y].rotate_right((t+1) * (t+2) / 2);
+            let temp = y;
+            y = (2*x + 3*y) % 5;
+            x = temp;
+        }
+    }
+
+    pub fn pi(&mut self) {
+        for x in 0..5 {
+            for y in 0..5 {
+                self.lanes[x][y] = self.lanes[(x + 3*y) % 5][x];
+            }
+        }
+    }
+
+    pub fn chi(&mut self) {
+        let mut new_lanes = [[0; 5]; 5];
+
+        for x in 0..5 {
+            for y in 0..5 {
+                new_lanes[x][y] = self.lanes[x][y] ^ 
+                    ((self.lanes[(x+1) % 5][y] ^ u64::MAX) & self.lanes[(x+2) % 5][y]);
+            }
+        }
+
+        self.lanes = new_lanes;
+    }
+    
+    // Round constant function for iota
+    pub fn rc(t: usize) {
+
     }
 }
 
