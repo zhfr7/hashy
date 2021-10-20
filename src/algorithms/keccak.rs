b16ca34e68301b328bdcfcc43ae2c5570385a349
Ahmad Zhafir <zzzhafir75@gmail.com>
2021-10-20 16:28:16 +0200

    Remove previously implemented step mapping functions
    Add separate round function that includes all of those functions.
    Add keccak_f_1600 function. Add related constants.

--- src/algorithms/keccak.rs
+++ src/algorithms/keccak.rs
@@ -1,8 +1,31 @@
 // References:
 // - https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf
-// - https://github.com/ctz/keccak/blob/master/keccak.py
+// - https://keccak.team/keccak_specs_summary.html
 
 type Lanes = [[u64; 5]; 5];
+
+const ROTATION_OFFSETS: [[u32; 5]; 5] = [
+    [0, 36, 3, 41, 18],
+    [1, 44, 10, 45, 2],
+    [62, 6, 43, 15, 61],
+    [28, 55, 25, 21, 56],
+    [27, 20, 39, 8, 14]
+];
+
+const ROUND_CONSTANTS: [u64; 24] = [
+    0x0000000000000001, 0x0000000000008082,
+    0x800000000000808A, 0x8000000080008000,
+    0x000000000000808B, 0x0000000080000001,
+    0x8000000080008081, 0x8000000000008009,
+    0x000000000000008A, 0x0000000000000088,
+    0x0000000080008009, 0x000000008000000A,
+    0x000000008000808B, 0x800000000000008B,
+    0x8000000000008089, 0x8000000000008003,
+    0x8000000000008002, 0x8000000000000080,
+    0x000000000000800A, 0x800000008000000A,
+    0x8000000080008081, 0x8000000000008080,
+    0x0000000080000001, 0x8000000080008008,
+];
 
 struct KeccakState {
     lanes: Lanes
@@ -45,65 +68,52 @@ impl KeccakState {
 
         out
     }
+}
 
-    // Step mapping functions
+fn keccak_f_1600(state: &mut KeccakState) {
+    for i in 0..24 {
+        round(state, ROUND_CONSTANTS[i]);
+    }
+}
 
-    pub fn phi(&mut self) {
-        let mut c = vec![];
-        let mut d = vec![];
+fn round(state: &mut KeccakState, round_constant: u64) {
+    // phi
+    let c: Vec<u64> = (0..5).map(|x| {
+        state.lanes[x][0] ^ 
+        state.lanes[x][1] ^ 
+        state.lanes[x][2] ^ 
+        state.lanes[x][3] ^ 
+        state.lanes[x][4]
+    }).collect();
 
-        for x in 0..5 {
-            c.push(self.lanes[x].iter().fold(0, 
-                |current, lane| current & lane));
-        }
+    let d: Vec<u64> = (0..5).map(|x| {
+        c[(x+4) % 5] ^ c[(x+1) % 5].rotate_right(1)
+    }).collect();
 
-        for x in 0..5 {
-            d.push(c[(x-1) % 5] ^ c[(x+1) % 5].rotate_right(1));
-        }
-
-        for x in 0..5 {
-            for y in 0..5 {
-                self.lanes[x][y] ^= d[x];
-            }
+    for x in 0..5 {
+        for y in 0..5 {
+            state.lanes[x][y] ^= d[x];
         }
     }
 
-    pub fn rho(&mut self) {
-        let (mut x, mut y) = (1, 0);
-
-        for t in 0..24 {
-            self.lanes[x][y] = self.lanes[x][y].rotate_right((t+1) * (t+2) / 2);
-            let temp = y;
-            y = (2*x + 3*y) % 5;
-            x = temp;
+    // rho and pi
+    let mut b = [[0; 5]; 5];
+    for x in 0..5 {
+        for y in 0..5 {
+            b[y][(2*x + 3*y) % 5] = 
+                state.lanes[x][y].rotate_right(ROTATION_OFFSETS[x][y]);
         }
     }
 
-    pub fn pi(&mut self) {
-        for x in 0..5 {
-            for y in 0..5 {
-                self.lanes[x][y] = self.lanes[(x + 3*y) % 5][x];
-            }
+    // chi
+    for x in 0..5 {
+        for y in 0..5 {
+            state.lanes[x][y] = b[x][y] ^ (!b[(x+1) % 5][y] & b[(x+2) % 5][y]);
         }
     }
 
-    pub fn chi(&mut self) {
-        let mut new_lanes = [[0; 5]; 5];
-
-        for x in 0..5 {
-            for y in 0..5 {
-                new_lanes[x][y] = self.lanes[x][y] ^ 
-                    ((self.lanes[(x+1) % 5][y] ^ u64::MAX) & self.lanes[(x+2) % 5][y]);
-            }
-        }
-
-        self.lanes = new_lanes;
-    }
-    
-    // Round constant function for iota
-    pub fn rc(t: usize) {
-
-    }
+    // iota
+    state.lanes[0][0] ^= round_constant;
 }
 
 #[cfg(test)]
@@ -115,9 +125,9 @@ mod test {
         let mut bytes = vec![128, 0, 0, 0, 0, 9, 0, 128];
         for _ in 0..192 { bytes.push(0) }
 
-        let keccak = KeccakState::from_bytes(&bytes);
-        println!("{:x}", keccak.lanes[0][0]);
-        assert_eq!(keccak.lanes[0][0], 0x8000000000090080);
+        let state = KeccakState::from_bytes(&bytes);
+        println!("{:x}", state.lanes[0][0]);
+        assert_eq!(state.lanes[0][0], 0x8000000000090080);
     }
 
     #[test]
@@ -125,7 +135,7 @@ mod test {
         let mut bytes = vec![128, 0, 0, 0, 0, 9, 0, 128];
         for _ in 0..192 { bytes.push(0) }
 
-        let keccak = KeccakState::from_bytes(&bytes);
-        assert_eq!(keccak.to_bytes(), bytes);
+        let state = KeccakState::from_bytes(&bytes);
+        assert_eq!(state.to_bytes(), bytes);
     }
 }
