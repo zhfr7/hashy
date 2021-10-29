5ba52e1e0cf01c149666af896ed072ce624f9cb3
Ahmad Zhafir <zzzhafir75@gmail.com>
2021-10-29 19:00:27 +0200

    Add SHA3 and SHAKE variants to router

--- src/router.rs
+++ src/router.rs
@@ -16,8 +16,21 @@ pub fn digest_from_algorithm(data: DataType, algorithm: String) -> Result<Vec<u8
         ["sha", "512"]  => sha2::digest(data, 512, 512),
         ["sha", "512", "224"]   => sha2::digest(data, 512, 224),
         ["sha", "512", "256"]   => sha2::digest(data, 512, 256),
+        ["sha3", "224"] => sha3::digest(data, 224),
+        ["sha3", "256"] => sha3::digest(data, 256),
+        ["sha3", "384"] => sha3::digest(data, 384),
+        ["sha3", "512"] => sha3::digest(data, 512),
+        ["shake128", len] => {
+            let out_len: usize = len.parse()?;
+            sha3::digest_shake_128(data, out_len)
+        },
+        ["shake256", len] => {
+            let out_len: usize = len.parse()?;
+            sha3::digest_shake_256(data, out_len)
+        }
+
         other => Err(anyhow::anyhow!(
-            format!("algorithm {} cannot be found", other.join("-"))
+            format!("Algorithm {} is currently unimplemented or non-existent", other.join("-"))
         ))
     }
 }
